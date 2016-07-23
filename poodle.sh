24810257d6889242818082bd5c121701e95c42d6
huggablehacker <huggablehacker@users.noreply.github.com>
2016-07-23 13:44:52 -0400

    Create poodle.sh

---
+++ poodle.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#set -x
+
+input=$1
+
+if [[ $input =~ : ]]; then
+host=$input
+else
+host=$input:443
+fi
+
+check=”`echo ‘x’ | openssl s_client -connect ${host} -quiet -ssl3 2> /dev/null`”
+if [ -z “${check}” ]; then
+echo “not vulnerable”
+else
+echo “vulnerable”
+fi
