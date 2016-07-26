9ecdd7473fa8ff5240e1cff457373fa943d6f665
Huggable Hacker <huggablehacker@users.noreply.github.com>
2016-07-26 19:14:47 -0400

    Update poodle.sh

--- poodle.sh
+++ poodle.sh
@@ -2,8 +2,8 @@
 
 if [ -z "$1" ]; then
 	echo "[*] NMAP Poodle Scan"
-	echo "[*] Usage	: $0 IP Address "
+	echo "[*] Usage	: $0 IP Address or Domain "
 	exit 0
 fi
 
-nmap -sV --version-light --script ssl-poodle -p 443 $1 >> $1-1.txt
+nmap -sV --version-light --script ssl-poodle -p 443 $1 >> $1.txt
